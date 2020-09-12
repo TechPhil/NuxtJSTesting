dbaecf5362cb00b2822870f6d45e3a30b5f8e45d
TechPhil <admin@techphil.xyz>
2020-09-12 01:07:25 +0100

    Imported NavBar into Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <NavBar/>
     <Nuxt />
   </div>
 </template>
+
+
+<script>
+import NavBar from "~/components/NavBar";
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 html {
